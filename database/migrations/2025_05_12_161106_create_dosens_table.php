5d666d1624120cb0ab9036203675e2c462639c45
KUBOJO <yakun079@gmail.com>
2025-05-13 00:31:19 +0800

    update fitur berita

---
+++ database/migrations/2025_05_12_161106_create_dosens_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+       Schema::create('dosens', function (Blueprint $table) {
+    $table->id();
+    $table->string('nama');
+    $table->string('nidn')->unique();
+    $table->string('bidang_keahlian');
+    $table->text('riwayat_pendidikan');
+    $table->text('pengalaman_kerja');
+    $table->text('penelitian_publikasi');
+    $table->string('foto')->nullable();
+    $table->string('email')->nullable();
+    $table->string('no_hp')->nullable();
+    $table->boolean('is_kaprodi')->default(false);
+    $table->timestamps();
+    
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dosens');
+    }
+};
